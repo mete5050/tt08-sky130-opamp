7b2cfc22cc0d8bf8a93c907fb69c55a1235fce44
mete5050 <33295066+mete5050@users.noreply.github.com>
2026-01-19 19:05:35 +0300

    Update project.v

--- src/project.v
+++ src/project.v
@@ -5,27 +5,27 @@
 
 `default_nettype none
 
-// DİKKAT: Modül ismi info.yaml'daki 'top_module' ile AYNI OLMALI
+// HATAYI ÇÖZEN SATIR BURASI: 'tt_um_example' yerine 'tt_um_mete_opamp' yazıyor.
 module tt_um_mete_opamp (
+    input  wire       VGND,
+    input  wire       VDPWR,    // 1.8v power supply
     input  wire [7:0] ui_in,    // Dedicated inputs
     output wire [7:0] uo_out,   // Dedicated outputs
     input  wire [7:0] uio_in,   // IOs: Input path
     output wire [7:0] uio_out,  // IOs: Output path
     output wire [7:0] uio_oe,   // IOs: Enable path (active high: 0=input, 1=output)
-    input  wire       ena,      // will go high when the design is enabled
+    inout  wire [7:0] ua,       // Analog pins
+    input  wire       ena,      // always 1 when the design is powered
     input  wire       clk,      // clock
     input  wire       rst_n     // reset_n - low to reset
 );
 
-  // Bu bir Analog proje.
-  // Dijital pinleri kullanmadığımız için hepsini 0'a (Logic Low) çekiyoruz.
-  // Bu sayede dijital testlerde hata almayız.
-
+  // Dijital çıkışları 0 yapıyoruz ki hata vermesin
   assign uo_out  = 8'b0;
   assign uio_out = 8'b0;
   assign uio_oe  = 8'b0;
 
-  // Kullanılmayan girişleri birleştirip 'unused' yapıyoruz ki uyarı vermesin
+  // Kullanılmayan girişleri susturuyoruz
   wire _unused = &{ui_in, uio_in, ena, clk, rst_n};
 
 endmodule
